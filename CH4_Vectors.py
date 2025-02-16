6f9b7ec7b013b3d8816a2b634e2b30a202e0b03d
arjaxa <ariana.kh21@gmail.com>
2025-02-16 10:48:28 +0330

    ch4-v-2

--- CH4_Vectors.py
+++ CH4_Vectors.py
@@ -15,4 +15,11 @@ grades = [95, #ex1
 def add(v: vector, w: vector) -> vector:
     """adds corresponding elements"""
     assert len(v) == len(w), "vectors must be the same lenght"
-    return []
+    return [v_i + w_i for v_i, w_i in zip(v, w)]
+assert add([1, 2, 3], [4, 5, 6]) == [5, 7, 9]
+# to substract 2 vectors, substract the corresponding elements
+def substract(v: vector, w: vector) -> vector:
+    """substracts corresponding elements"""
+    assert len(v) == len(w), "vectors must be the same lenght"
+    return [v_i - w_i for v_i, w_i in zip(v, w)]
+assert substract([5, 7, 9], [4, 5, 6]) == [1, 2, 3]
