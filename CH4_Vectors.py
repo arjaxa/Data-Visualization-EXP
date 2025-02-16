d71ea98c1e95185e5728e341882f0fbec97a3b98
arjaxa <ariana.kh21@gmail.com>
2025-02-16 12:47:27 +0330

    ch4-2

--- CH4_Vectors.py
+++ CH4_Vectors.py
@@ -1,4 +1,4 @@
-from matplotlib import pyplot as plt
+
 from typing import List
 
 vector = list[float]
@@ -23,3 +23,19 @@ def substract(v: vector, w: vector) -> vector:
     assert len(v) == len(w), "vectors must be the same lenght"
     return [v_i - w_i for v_i, w_i in zip(v, w)]
 assert substract([5, 7, 9], [4, 5, 6]) == [1, 2, 3]
+
+# 2
+
+def vector_sum(vectors: list[vector]) -> vector:
+    """sums all the corresponding elements"""
+    # check that vectors is not empty
+    assert vectors, "no vectors provided!"
+
+    # check the vectors are all the same size
+    num_elements = len(vectors[0])
+    assert all(len(v) == num_elements for v in vectors), "different sizes!"
+
+    # the i_th element of the result is the sum of every vector[i]
+    return [sum(vector[i] for vector in vectors)
+            for i in range(num_elements)]
+assert vector_sum([[1, 2], [3, 4], [5, 6], [7, 8]]) == [16, 20]
