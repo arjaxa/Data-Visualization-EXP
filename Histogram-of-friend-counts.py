fdd26d7cd9e4c9e6c0697e6e8ca0463d36281186
arjaxa <ariana.kh21@gmail.com>
2025-02-27 15:06:21 +0330

    f5-1

--- Histogram-of-friend-counts.py
+++ Histogram-of-friend-counts.py
@@ -1,2 +1,15 @@
 # chapter 5 / describing a single set of data
 
+from matplotlib import pyplot as plt
+from collections import Counter
+import matplotlib.pyplot as plt
+
+friend_counts = Counter(num_friends)
+xs = range(101) #largest value is 100
+ys = [friend_counts[x] for x in xs] # height is just # of friends
+plt.bar(xs, ys)
+plt.axis([0, 101, 0, 25])
+plt.title("Histogram of Friend Counts")
+plt.xlabel("# of friends")
+plt.ylabel("# of people")
+plt.show()
