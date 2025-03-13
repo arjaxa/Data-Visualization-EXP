555a2f52d437d44a247a2b8bd6f49a9ddfaf3be4
arjaxa <ariana.kh21@gmail.com>
2025-03-13 14:39:25 +0330

    e3

--- Binomial-Histogram6-4.py
+++ Binomial-Histogram6-4.py
@@ -10,6 +10,19 @@ def binomial(n: int, p: float) -> int:
     """returns the sum of n bernoulli(p) trials"""
     return sum(bernoulli_trial(p) for _ in range(n))
 
+from collections import Counter
+
+def binomial_histogram(p: float, n: int, num_points: int) -> None:
+    """picks points from a binomial(n,p) and plots their histogram"""
+    data = [binomial(n, p) for _ in range(num_points)]
+
+    # bar chart to show the actual binomial samples
+    histogram = Counter(data)
+    plt.bar([x - 0.4 for x in histogram.keys()],
+            [v / num_points for v in histogram.values()],
+            0.8,
+            color='0.75')
+
 import matplotlib.pyplot as plt
 
 xs = [x / 10.0 for x in range(-50, 50)]
