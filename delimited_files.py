59c22b67d5ad4c054c3611b48475215921d105bb
arjaxa <ariana.kh21@gmail.com>
2025-03-31 07:59:31 +0330

    s

---
+++ delimited_files.py
@@ -0,0 +1,9 @@
+import csv
+
+with open('tab_delimited_stock_prices.txt') as f:
+    tab_reader = csv.reader(f, delimiter='\t')
+    for row in tab_reader:
+        date = row[0]
+        symbol = row[1]
+        closing_price = float(row[2])
+        process(date, symbol, closing_price)
