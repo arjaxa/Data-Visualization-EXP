71dab3d2d14484dd8372b5b4c63cb03508dcdb4b
arjaxa <ariana.kh21@gmail.com>
2025-03-31 08:25:08 +0330

    s

--- web_scrap.py
+++ web_scrap.py
@@ -2,3 +2,5 @@ from bs4 import BeautifulSoup
 import requests
 
 url = ("https://raw.githubusercontent.com/joelgrus/data/master/getting-data.html")
+html = requests.get(url).text
+soup = BeautifulSoup(html, 'html5lib')
