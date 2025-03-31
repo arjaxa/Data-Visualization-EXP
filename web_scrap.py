372ee6f705da0e43e9b1b1b4123a61af647c43c7
arjaxa <ariana.kh21@gmail.com>
2025-03-31 08:39:25 +0330

    s

--- web_scrap.py
+++ web_scrap.py
@@ -4,3 +4,19 @@ import requests
 url = ("https://raw.githubusercontent.com/joelgrus/data/master/getting-data.html")
 html = requests.get(url).text
 soup = BeautifulSoup(html, 'html5lib')
+
+first_paragraph = soup.find('p') # or soup.p
+first_paragraph_text = soup.p.text
+first_paragraph_words = soup.p.text.split()
+
+# extracting a tag's attributes by treating it like a dict
+
+first_paragraph_id = soup.p['id'] # raises keyError if no 'id'
+first_paragraph_id2 = soup.p.get('id') # returns None if no 'id'
+
+# to get multiple tags at once
+
+all_paragraphs = soup.find_all('p') # or just soap('p')
+paragraphs_with_ids = [p for p in soup('p') if p.get('id')]
+
+# getting tags with a specific class
