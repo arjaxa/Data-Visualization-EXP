9c5e5fb5be4a211dc615e1bc02d00d07e76309aa
arjaxa <ariana.kh21@gmail.com>
2025-05-16 13:54:13 +0330

    irs

--- IrisDataset-ML.py
+++ IrisDataset-ML.py
@@ -1,7 +1,13 @@
 import requests
 from typing import Dict
+from typing import NamedTuple
 import csv
 from collections import defaultdict
+from matplotlib import pyplot as plt
+
+class LabelPoint(NamedTuple):
+    point: Vector
+    label: str
 
 def parse_iris_row(row: list[str]) -> LabeledPoint:
     measurements = [float(value) for value in row[:-1]]
@@ -12,7 +18,9 @@ with open('iris.data') as f:
     reader = csv.reader(f)
     iris_data = [parse_iris_row(row) for row in reader]
 
-
+points_by_species: Dict[str, list[Vector]] == defaultdict(list)
+for iris in iris_data:
+    points_by_species[iris.label].append(iris.point)
 
 data = requests.get("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")
 
