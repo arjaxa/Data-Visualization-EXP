9552cf4da26a5b2cfc73fbe3680595ad0b147771
arjaxa <ariana.kh21@gmail.com>
2025-06-01 10:01:53 +0330

    2

--- SpamFilter.py
+++ SpamFilter.py
@@ -1,7 +1,14 @@
 from typing import Set
 import re
+from typing import NamedTuple
 
 def tokenize(text: str) -> set[str]:
     text = text.lower() # convert to lowercase
     all_words = re.findall("[a-z0-9]+", text) # extract the words, and
     return set(all_words) # remove duplicates
+
+assert tokenize("Data Science is science") == {"data", "science", "is"}
+
+class Message(NamedTuple):
+    text: str
+    is_spam: bool
