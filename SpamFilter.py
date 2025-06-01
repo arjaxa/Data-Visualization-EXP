b8468cb90c025c6eb9adb4e9ddd373ce11106bc3
arjaxa <ariana.kh21@gmail.com>
2025-06-01 10:13:50 +0330

    3

--- SpamFilter.py
+++ SpamFilter.py
@@ -1,6 +1,9 @@
 from typing import Set
 import re
 from typing import NamedTuple
+from typing import List, Tuple, Dict, Iterable
+import math
+from collections import defaultdict
 
 def tokenize(text: str) -> set[str]:
     text = text.lower() # convert to lowercase
@@ -12,3 +15,12 @@ assert tokenize("Data Science is science") == {"data", "science", "is"}
 class Message(NamedTuple):
     text: str
     is_spam: bool
+
+    class NaiveBayesClassifier:
+        def __init__(self, k: float = 0.5) -> None:
+            self.k = k # smoothing factor
+
+            self.tokens: set[str] = set()
+            self.token_spam_counts: Dict[str, int] = defaultdict(int)
+            self.token_ham_counts: Dict[str, int] = defaultdict(int)
+            self.spam_messages = self.ham_messages = 0
